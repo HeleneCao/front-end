11dca9d0059b4bc3638ebb1f0f2e5ad9eddc7b40
Miloud <miloud.charef00@gmail.com>
2023-04-20 14:27:59 +0200

    liste des intern de la team

--- src/pages/TeamDetails.jsx
+++ src/pages/TeamDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { teamService } from "../service/team.service";
-import { useParams } from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 const TeamDetails = () => {
   let { uuid } = useParams();
@@ -23,6 +23,7 @@ const TeamDetails = () => {
   }
 
   return (
+      <div>
     <table className="min-w-full divide-y divide-gray-200">
       <thead className="bg-gray-50">
         <tr>
@@ -50,10 +51,58 @@ const TeamDetails = () => {
         <tr key={team.uuid}>
           <td className="px-6 py-4 whitespace-nowrap">{team.name}</td>
           <td className="px-6 py-4 whitespace-nowrap">{team.skills.map((skill) => skill.label).join(" - ")}</td>
-          <td className="px-6 py-4 whitespace-nowrap">{team.nbrIntern}</td>
+          <td className="px-6 py-4 whitespace-nowrap">{team.interns.length}</td>
         </tr>
       </tbody>
     </table>
+      <div className="p-28">
+        <div className="overflow-x-auto">
+          <div className="p-3 bg-blue-500 text-center border border-blue-500 rounded-t-2xl">
+           <h2 className="font-mono not-italic font-bold text-2xl leading-9 text-white">Liste des Interns de la Team</h2>
+         </div>
+         <table className="w-full border-slate-300">
+            <thead className="text-left border border-t-gray-300">
+           <tr>
+             <th scope="col">Nom
+              </th>
+              <th scope="col">Prenom
+             </th>
+             <th scope="col">adresse mail
+             </th>
+             <th scope="col">
+
+             </th>
+           </tr>
+            </thead>
+            <tbody>
+        {team.interns.map((intern,index) => (
+            <tr key={index}>
+              <td>{intern.firstName}</td>
+              <td>{intern.lastName}</td>
+              <td>{intern.email}</td>
+              <td className="text-center">
+                <button>
+                  <img
+                      src="..\src\images\Vector.png"
+                      alt="Bouton archivé"
+                      className="h-3 w-auto mr-2"
+                  />
+                </button>
+              </td>
+            </tr>
+        ))
+        }
+        </tbody>
+      </table>
+      <div className="p-7 grid place-items-end">
+        <button
+            className="border-2 border-blue-500 rounded-full p-1 px-2 flex items-end">
+          Ajouter un membre
+        </button>
+      </div>
+    </div>
+    </div>
+</div>
   );
 };
 
